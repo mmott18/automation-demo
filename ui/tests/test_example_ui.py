5e7c7f156091af4dda2b7f733c0f900153625ed5
Matt <matthewmott7@gmail.com>
2025-05-14 19:35:32 -0400

    Create test_example_ui.py

---
+++ ui/tests/test_example_ui.py
@@ -0,0 +1,9 @@
+from playwright.sync_api import sync_playwright
+
+def test_example_ui():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=True)
+        page = browser.new_page()
+        page.goto("https://example.com")
+        assert page.title() == "Example Domain"
+        browser.close()
